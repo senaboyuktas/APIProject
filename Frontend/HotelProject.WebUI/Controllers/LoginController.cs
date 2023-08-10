247881472a0de70a0f704186aa5a7349253389f6
senaboyuktas <senabykts99@gmail.com>
2023-08-10 19:24:13 +0300

    Final

--- Frontend/HotelProject.WebUI/Controllers/LoginController.cs
+++ Frontend/HotelProject.WebUI/Controllers/LoginController.cs
@@ -32,7 +32,7 @@ namespace HotelProject.WebUI.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Staff");
+                    return RedirectToAction("Index", "Dashboard");
                 }
 
                 else
